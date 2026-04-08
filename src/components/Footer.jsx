7c94328ad22927575a6bbb9c4dedf67a8a66bcf6
Cristian Javier <cristian.padilla6298@alumnos.udg.mx>
2026-04-08 01:54:15 -0600

    feat: agrega imagen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
+import logo from "../img/logo.png";
+
 function Footer() {
   return (
     <footer className="footer">
+      <img src={logo} alt="Logo" className="footer-img" />
+      
       <p><strong>Integrante:</strong> Cristian Javier Padilla Cornejo</p>
       <p><strong>Profesor:</strong> Zeus Emanuel Gutierrez Cobian</p>
       <p><strong>Materia:</strong> Desarrollo de Aplicaciones Web en la Nube y Móviles</p>
