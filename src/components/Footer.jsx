57dbfeaeec973dab3e3d08cc86e4866ea7d1fb8f
Cristian Javier <cristian.padilla6298@alumnos.udg.mx>
2026-04-08 02:05:47 -0600

    style: centra imagen en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import logo from "../img/logo.png";
 function Footer() {
   return (
     <footer className="footer">
-      <img src={logo} alt="Logo" className="footer-img" />
       
       <p><strong>Integrante:</strong> Cristian Javier Padilla Cornejo</p>
       <p><strong>Profesor:</strong> Zeus Emanuel Gutierrez Cobian</p>
       <p><strong>Materia:</strong> Desarrollo de Aplicaciones Web en la Nube y Móviles</p>
       <p><strong>Calendario:</strong> 2026A</p>
       <p><strong>Fecha:</strong> Abril 2026</p>
-      <p><strong>Centro universitario:</strong> CUCEI - Universidad de Guadalajara</p>
+      <p><strong>Institución:</strong> CUCEI - Universidad de Guadalajara</p>
+      <img src={logo} alt="CUCEI" className="footer-img" />
     </footer>
   );
 }
